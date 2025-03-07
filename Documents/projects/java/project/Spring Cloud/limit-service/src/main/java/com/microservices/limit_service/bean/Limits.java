f57e16166a3ec781deec9a073ee12b7d36964c18
Shivam Patel <shivampatelcodes01@gmail.com>
2025-03-07 12:32:27 -0500

    create a limit service config

--- Documents/projects/java/project/Spring Cloud/limit-service/src/main/java/com/microservices/limit_service/bean/Limits.java
+++ Documents/projects/java/project/Spring Cloud/limit-service/src/main/java/com/microservices/limit_service/bean/Limits.java
@@ -1,8 +1,12 @@
 package com.microservices.limit_service.bean;
+import lombok.*;
 
-
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class Limits {
 
+    private int minimum;
     private int maximum;
-    private int minimun;
 }
--- Documents/projects/java/project/Spring Cloud/limit-service/src/main/java/com/microservices/limit_service/controller/LimitsController.java
+++ Documents/projects/java/project/Spring Cloud/limit-service/src/main/java/com/microservices/limit_service/controller/LimitsController.java
@@ -1,14 +1,21 @@
 package com.microservices.limit_service.controller;
 
-import com.microservices.limit_service.Limits;
+import com.microservices.limit_service.bean.Limits;
+import com.microservices.limit_service.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public Limits retrieveLimits(){
-
+        return new Limits(configuration.getMinimum(),
+                    configuration.getMaximum());
     }
 }
